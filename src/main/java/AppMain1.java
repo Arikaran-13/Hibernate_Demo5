c7ae3cf3c19fe7db371eaaee91beddbd6a4a3838
Arikaran S <91272168+Arikaran-13@users.noreply.github.com>
2022-07-01 17:15:38 +0530

    self referencing relationship

--- src/main/java/AppMain1.java
+++ src/main/java/AppMain1.java
@@ -11,7 +11,7 @@ public class AppMain1 {
         Author a = new Author();
         a.setName("james cartner");
         a.setId(101);
-        a.setSponser_person(a);
+        a.setSponser_person(a); // self referencing 
         Sponser s = new Sponser();
         s.setSponser_id(1);
         s.setSponser_name("jamesc");
